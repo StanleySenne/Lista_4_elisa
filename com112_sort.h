afb54e72b5e92327fc16a21a7a09bbaf8f89320b
StanleySenne <56733982+StanleySenne@users.noreply.github.com>
2020-05-08 18:01:05 -0300

    Create com112_sort.h

---
+++ com112_sort.h
@@ -0,0 +1,8 @@
+#ifndef C0M112_SORT_H
+#define	C0M112_SORT_H
+
+void bubbleSort(int *vetor, int tam, int *comp, int * mov);
+void selectionSort(int *vetor, int tam, int *comp, int * mov);
+void insertionSort(int *vetor, int tam, int *comp, int * mov);
+
+#endif
